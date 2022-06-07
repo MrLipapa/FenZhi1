783392554200bc456830c748297dd5c26ea3b1fd
Kang <1289917320@qq.com>
2022-06-07 11:07:58 +0800

    Kang

---
+++ src/main/java/org/example/fenzhi1/kangshuaiqi/Kangshuaiqi.java
@@ -0,0 +1,7 @@
+package org.example.fenzhi1.kangshuaiqi;
+
+public class Kangshuaiqi {
+    public static void main(String[] args) {
+        System.out.println("康帅气");
+    }
+}
---
+++ src/main/java/org/example/fenzhi1/kangshuaiqi/Wangweiwei.java
@@ -0,0 +1,7 @@
+package org.example.fenzhi1.kangshuaiqi;
+
+public class Wangweiwei {
+    public static void main(String[] args) {
+        System.out.println("www");
+    }
+}
